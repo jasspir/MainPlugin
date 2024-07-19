6b227f7287382eda6f725ec756e669a6771ce5d6
Jass <145933956+jasspir@users.noreply.github.com>
2024-07-18 21:07:35 -0400

    Update MainPlugin.java

--- src/main/java/me/jass/mainplugin/MainPlugin.java
+++ src/main/java/me/jass/mainplugin/MainPlugin.java
@@ -1,22 +1,36 @@
 package me.jass.mainplugin;
 
 import me.jass.api.API;
+import me.jass.api.SneakEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Level;
-
-public final class MainPlugin extends JavaPlugin {
+public final class MainPlugin extends JavaPlugin implements Listener {
     private External api;
+    public static String name = "clementine";
 
     @Override
     public void onEnable() {
         api = new External();
         Bukkit.getServicesManager().register(API.class, api, this, ServicePriority.Normal);
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
+    }
+
+    @EventHandler
+    public void sneakEvent(PlayerToggleSneakEvent event) {
+        //this will be called when a player toggles sneak
+        boolean sneaking = event.isSneaking();
+
+        //now we make our own custom sneak event
+        SneakEvent sneakEvent = new SneakEvent(sneaking);
+        Bukkit.getPluginManager().callEvent(sneakEvent);
     }
 }
